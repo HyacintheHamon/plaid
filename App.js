84b2bf3228ae3b8c59fed58bc3ad880ca5dcf64b
HyacintheHamon <hamon.hyacinthe@gmail.com>
2019-10-20 15:14:58 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import PlaidAuthenticator from 'react-native-plaid-link';
+
+export default class App extends Component {
+  state = {
+    data: {},
+    status: 'LOGIN_BUTTON'
+  };
+
+  render() {
+    console.log(this.state.status)
+
+    switch(this.state.status) {
+      case 'CONNECTED':
+        console.log('connected')
+        return this.renderDetails()
+      case 'LOGIN_BUTTON':
+      case 'EXIT':
+        return this.renderButton();
+      default:
+        return this.renderLogin();
+    }
+  }
+
+  renderButton = () => {
+    return <View style={styles.container}>
+      <TouchableOpacity onPress={() => this.setState({status: ''})}>
+        <Text style={styles.paragraph}>Login with Plaid</Text>
+      </TouchableOpacity>
+    </View>
+  }
+
+  onLoadStart = props => {
+    console.log('onLoadStart', props);
+  };
+
+  onLoad = props => {
+    console.log('onLoad', props);
+  };
+
+  onLoadEnd = props => {
+    console.log('onLoadEnd', props);
+  };
+
+  renderLogin() {
+    return (
+      <PlaidAuthenticator
+        onMessage={this.onMessage}
+        publicKey="e382467e0f5669cd67d8826ff1c089"
+        env="sandbox"
+        product="auth,transactions"
+        onLoad={this.onLoad}
+        onLoadStart={this.onLoadStart}
+        onLoadEnd={this.onLoadEnd}
+      />
+    );
+  }
+
+  renderDetails() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>Institution</Text>
+        <Text style={styles.value}>
+          {this.state.data.metadata.institution.name}
+        </Text>
+        <Text style={styles.paragraph}>Institution ID</Text>
+        <Text style={styles.value}>
+          {this.state.data.metadata.institution.institution_id}
+        </Text>
+        <Text style={styles.paragraph}>Token</Text>
+        <Text style={styles.value}>
+          {this.state.data.metadata.public_token}
+        </Text>
+      </View>
+    );
+  }
+
+  onMessage = data => {
+    // console.log(data)
+    /*
+      Response example for success
+
+      {
+        "action": "plaid_link-undefined::connected",
+        "metadata": {
+          "account": {
+            "id": null,
+            "name": null
+          },
+          "account_id": null,
+          "public_token": "public-sandbox-e697e666-9ac2-4538-b152-7e56a4e59365",
+          "institution": {
+            "name": "Chase",
+            "institution_id": "ins_3"
+          }
+        }
+      }
+    */
+
+    this.setState({ data, status: data.action.substr(data.action.lastIndexOf(':') + 1).toUpperCase() });
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 24,
+    backgroundColor: '#ecf0f1'
+  },
+  paragraph: {
+    fontSize: 18,
+    marginBottom: 5,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e'
+  },
+  value: {
+    marginBottom: 20,
+    textAlign: 'center'
+  }
+});
